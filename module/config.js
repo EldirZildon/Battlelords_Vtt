b032c037c50486aab5020752e836dd6e9f646b7a
Eldir_zildon <Drummerkeith56@yahoo.com>
2024-06-09 20:11:25 -0400

    added in language

---
+++ module/config.js
@@ -0,0 +1,28 @@
+export const blcr = {};
+
+blcr.stat={
+    str: {
+        label: "blcr.stat.str.label",
+        abbreviation: "blcr.stat.str.abbreviation",
+      },
+      dex: {
+        label: "blcr.stat.dex.label",
+        abbreviation: "blcr.stat.dex.abbreviation"
+      },
+      con: {
+        label: "blcr.stat.con.label",
+        abbreviation: "blcr.stat.con.abbreviation"
+      },
+      int: {
+        label: "blcr.stat.int.label",
+        abbreviation: "blcr.stat.int.abbreviation"
+      },
+      wis: {
+        label: "blcr.stat.wis.label",
+        abbreviation: "blcr.stat.wis.abbreviation"
+      },
+      cha: {
+        label: "blcr.stat.cha.label",
+        abbreviation: "blcr.stat.cha.abbreviation"
+      },
+}
