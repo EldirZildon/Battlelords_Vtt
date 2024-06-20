b842361ca107b49ead37484bc4ec7097933b327b
Eldir_zildon <Drummerkeith56@yahoo.com>
2024-06-19 21:36:04 -0400

    stat mkII

--- src/datamodels/module/data/item-item.mjs
+++ src/datamodels/module/data/item-item.mjs
@@ -13,7 +13,7 @@ export default class BattlelordsItem extends BattlelordsItemBase {
     // Break down roll formula into three independent fields
     schema.roll = new fields.SchemaField({
       diceNum: new fields.NumberField({ ...requiredInteger, initial: 1, min: 1 }),
-      diceSize: new fields.StringField({ initial: "d20" }),
+      diceSize: new fields.StringField({ initial: "d100" }),
       diceBonus: new fields.StringField({ initial: "+@str.mod+ceil(@lvl / 2)" })
     })
 
